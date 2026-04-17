b78cf9bc5fe563e44857c2b5d48df4a025ef7ece
lermnote <hanlong@live.cn>
2026-04-17 11:09:13 +0800

    refactor: move config to local env file

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 // app.js
+const localConfig = require('./config/env.js')
+
 App({
   globalData: {
-    env: 'lerm-stn-5gnvxp7fecfa15ef',
     pendingUrl: null
   },
   onLaunch: function () {
@@ -9,18 +10,15 @@ App({
       console.error("请使用 2.2.3 或以上的基础库以使用云能力");
     } else {
       wx.cloud.init({
-        env: this.globalData.env,
+        env: localConfig.cloudEnv,
         traceUser: true
       })
-
     }
   },
   onShow(options) {
     // 接收从微信内分享过来的链接
     if (options.referrerInfo && options.referrerInfo.extraData) {
-      const {
-        url
-      } = options.referrerInfo.extraData
+      const { url } = options.referrerInfo.extraData
       if (url) {
         // 存到全局，clip 页面 onShow 时读取
         this.globalData.pendingUrl = url
---
+++ config/env.example.js
@@ -0,0 +1,7 @@
+/**
+ * 本地环境配置（勿提交到 Git）
+ * 使用前请先在 project.private.config.json 中配置云环境
+ */
+module.exports = {
+  cloudEnv: 'your-cloud-env-id',
+}
